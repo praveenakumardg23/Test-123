5ba17cc3bfaf8474df9e0a52bd9bca41c20e653d
Praveena Kumar <pkumar@i3verticals.com>
2024-02-15 20:01:49 +0530

    Test App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'PSMupgrade',
+  appName: 'Test123upgrade',
   webDir: 'www',
   server: {
     androidScheme: 'https'
